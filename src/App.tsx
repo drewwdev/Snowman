91068ea9d85797004b649fdc0e336761a54a72a7
Drew Wilson <jankyy93@gmail.com>
2021-09-13 14:23:04 -0400

    static html finished. added images folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 export function App() {
-  return <div>Hello, World</div>
+  const [word, setWord] = useState([
+    'abdomen',
+    'aborted',
+    'abought',
+    'achiest',
+    'acolyte',
+    'aconite',
+    'acrogen',
+    'adipose',
+    'adopter',
+    'advisor',
+    'aileron',
+    'ailment',
+    'albumen',
+    'albumin',
+    'aliment',
+    'alimony',
+    'almoner',
+    'alright',
+    'ambient',
+    'ambling',
+  ])
+  const alphabet = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ]
+  return (
+    <div>
+      <main>
+        <header>Snowman!</header>
+        <img src="/images/step_7.png" />
+        <nav>_ _ _ _ _ _ _</nav>
+      </main>
+      <footer>{alphabet}</footer>
+    </div>
+  )
 }
